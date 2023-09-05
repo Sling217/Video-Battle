16aba95eb5a3105ee6bbbc15a08e75e12f966b2e
Kriss Strikis <kstrikis@gmail.com>
2023-09-05 19:39:36 -0400

    add api endpoint, frontend fetches

--- client/src/components/VideoEmbed.js
+++ client/src/components/VideoEmbed.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import ReactPlayer from 'react-player'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const VideoEmbed = (props) => {
-    const [url, setUrl] = useState("https://www.youtube.com/watch?v=NT5-XrNlVNE")
+    const [url, setUrl] = useState("")
     const [videoLink, setVideoLink] = useState("")
 
+    const getVideoLink = async () => {
+        try {
+            const response = await fetch("/api/v1/videoLinks")
+            const responseBody = await response.json()
+            if (!response.ok) {
+                throw new Error(`${response.status} (${response.statusText})`)
+            }
+            setUrl(responseBody.videoLink.fullUrl)
+        } catch(err) {
+            console.error("Error in fetch", err.message)
+        }
+    }
+
+    const postNewVideoLink = async () => {
+        try {
+            const response = await fetch("api/v1/videoLinks", {
+                method: "POST",
+                headers: new Headers({
+                    "Content-Type": "application/json"
+                }),
+                body: JSON.stringify( {videoLink: videoLink})
+            })
+            if (!response.ok) {
+                const errorMessage = await response.json()
+                throw new Error(errorMessage)
+            }
+        } catch(err) {
+            console.error("Error in fetch", err.message)
+        }
+    }
+
+    useEffect(() => {
+        getVideoLink()
+    }, [])
+    
     const handleInputChange = (event) => {
         setVideoLink(    
             event.currentTarget.value
         )
     }
+
     const handleSubmit = (event) => {
         event.preventDefault()
+        postNewVideoLink()
         setUrl(videoLink)
     }
 
---
+++ server/src/routes/api/v1/videoLinksRouter.js
@@ -0,0 +1,37 @@
+import express from "express"
+import { VideoLink } from "../../../models/index.js"
+import { ValidationError } from "objection"
+import cleanUserInput from "../../../services/cleanUserInput.js"
+
+const videoLinksRouter = new express.Router()
+
+videoLinksRouter.get("/", async (req, res) => {
+    try {
+        const videoFullUrl = await VideoLink.query().orderBy("updatedAt", 'desc').limit(1).first()
+        res.status(200).json({ videoLink: videoFullUrl })
+    } catch(err) {
+        res.status(500).json( {errors: err.message } )
+    }
+})
+
+videoLinksRouter.post("/", async (req, res) => {
+    try {
+        const { body } = req
+        const cleanedInput = cleanUserInput(body.videoLink)
+        const userId = req.user.id
+        const newVideoLinkObject = {
+            fullUrl: cleanedInput,
+            userId: userId
+        }
+        const videoLink = await VideoLink.query().insertAndFetch(newVideoLinkObject)
+        res.set({"Content-Type": "application/json"}).status(201).json({ videoLink: videoLink })
+    } catch(err) {
+        if (err instanceof ValidationError) {
+            res.status(422).json({ errors: err.data })
+        } else {
+            res.status(500).json({ errors: err.message })
+        }
+    }
+})
+
+export default videoLinksRouter
