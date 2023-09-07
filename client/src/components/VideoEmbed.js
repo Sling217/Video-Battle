937a26b688262662b78beb96c713c0ac9b9c2551
Kriss Strikis <kstrikis@gmail.com>
2023-09-07 18:35:16 -0400

    start video playing with delay since submission

--- client/src/components/VideoEmbed.js
+++ client/src/components/VideoEmbed.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactPlayer from 'react-player'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const VideoEmbed = (props) => {
     const [url, setUrl] = useState("")
     const [videoLink, setVideoLink] = useState("")
+    const [seekTime, setSeekTime] = useState(0)
+    const playerRef = useRef()
 
     const getVideoLink = async () => {
         try {
@@ -14,6 +16,10 @@ const VideoEmbed = (props) => {
                 throw new Error(`${response.status} (${response.statusText})`)
             }
             setUrl(responseBody.videoLink.fullUrl)
+            const videoTime = new Date(responseBody.videoLink.updatedAt)
+            const currentTime = new Date()
+            const timeElapsed = (currentTime - videoTime) / 1000
+            setSeekTime(timeElapsed)
         } catch(err) {
             console.error("Error in fetch", err.message)
         }
@@ -41,6 +47,12 @@ const VideoEmbed = (props) => {
         getVideoLink()
     }, [])
     
+    useEffect(() => {
+        if(playerRef.current) {
+            playerRef.current.seekTo(seekTime)
+        }
+    }, [seekTime])
+    
     const handleInputChange = (event) => {
         setVideoLink(    
             event.currentTarget.value
@@ -55,7 +67,14 @@ const VideoEmbed = (props) => {
 
     return (
         <div>
-            <ReactPlayer url={url} />
+            <ReactPlayer
+                ref={playerRef}
+                controls={true}
+                volume={null}
+                muted={true}
+                playing={true}
+                url={url}
+            />
             <form onSubmit={handleSubmit}>
                 <label htmlFor='videoLink'>
                     Video Link
