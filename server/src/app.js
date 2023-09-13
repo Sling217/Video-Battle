46c68cf373b76ea9edf924a0897a0323abcf74f3
Kriss Strikis <kstrikis@gmail.com>
2023-09-13 19:46:39 -0400

    clean up server initialization

--- server/src/app.js
+++ server/src/app.js
@@ -11,20 +11,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 import hbsMiddleware from "express-handlebars";
-
 import WebSocket from 'ws'
 
 const wss = new WebSocket.Server({ port: 8080 })
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    wss.clients.forEach(client => {
-      console.log("testing2", message)
-    })
-  })
-  ws.on('open', open => {
-    console.log("open thing received: ", open.data)
-  })
-})
 
 app.set("views", path.join(__dirname, "../views"));
 app.engine(
